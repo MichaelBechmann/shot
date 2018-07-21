1aaaa31eef03fa3d2ecf3a3b8304b55d338b4790
Michael Bechmann <michael_bechmann@yahoo.de>
2018-07-21 18:04:30 +0200

    intermediate

--- controllers/components.py
+++ controllers/components.py
@@ -12,11 +12,4 @@ if 0:
 from shotdbutil import Events
 
 def schedule():
-    announcements = Events(shotdb).get_visible()
-    if announcements:
-        return TABLE([TD('%s am %s, %s' % (r.label, r.date, r.time)) for r in announcements], _id="tbl_next_event_date")
-
-    else:
-        return SPAN('Die Termine für die nächsten Märkte stehen noch nicht fest.')
-
-    return dict(heading='Alle Termine')
+    return dict(announcements = Events(shotdb).get_visible())
