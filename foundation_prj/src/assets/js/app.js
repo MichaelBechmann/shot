5f7d8a26508b1f195c681a882dc0657a4c6f5660
Michael Bechmann <michael_bechmann@yahoo.de>
2018-10-30 17:19:32 +0100

    intermediate

--- foundation_prj/src/assets/js/app.js
+++ foundation_prj/src/assets/js/app.js
@@ -8,15 +8,89 @@ import Foundation from 'foundation-sites';
 // the line below
 //import './lib/foundation-explicit-pieces';
 
+import 'tablesaw/dist/tablesaw.jquery';
+import libs from './lib/dependencies';
+window.libs = libs;
 
 $(document).foundation();
 
+libs.AOS.init();
 
-// Add and remove a class to the off-canvas elements (menu, schedule)
-// if the size of the title bar changes.
-// With the class the off-canvas elements can be shifted up and down accordingly.
-$('.top-bar').on('sticky.zf.stuckto:top', function(){
-  $('.off-canvas, #top-bar-wrapper').addClass('title-bar-slim');
-}).on('sticky.zf.unstuckfrom:top', function(){
-  $('.title-bar-slim').removeClass('title-bar-slim');
-})
+// SVG Injector
+// Elements to inject
+var mySVGsToInject = document.querySelectorAll('img.inject-me');
+
+// Options
+var injectorOptions = {
+  evalScripts: 'once',
+  pngFallback: 'assets/png'
+};
+
+var afterAllInjectionsFinishedCallback = function (totalSVGsInjected) {
+  // Callback after all SVGs are injected
+  console.log('We injected ' + totalSVGsInjected + ' SVG(s)!');
+};
+
+var perInjectionCallback = function (svg) {
+  // Callback after each SVG is injected
+  console.log('SVG injected: ' + svg);
+};
+
+// create injector configured by options
+var injector = new libs.svgInjector(injectorOptions);
+
+// Trigger the injection
+injector.inject(
+  mySVGsToInject,
+  afterAllInjectionsFinishedCallback,
+  perInjectionCallback
+);
+
+// slick carousel
+$(".content-carousel").slick({
+  // normal options...
+  speed: 5000,
+	autoplay: true,
+	autoplaySpeed: 0,
+	cssEase: 'linear',
+  slidesToShow: 5,
+	slidesToScroll: 1,
+  infinite: true,
+  swipeToSlide: true,
+	centerMode: true,
+  focusOnSelect: true,
+  // the magic
+  responsive: [{
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        infinite: true
+      }
+    }, {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        dots: true
+      }
+    }, {
+      breakpoint: 300,
+      settings: "unslick" // destroys slick
+    }]
+});
+
+// tablesaw table plugin
+$(function () {
+  $(document)
+    .foundation()
+    .trigger('enhance.tablesaw');
+});
+
+var TablesawConfig = {
+  swipeHorizontalThreshold: 15
+};
+
+// app dashboard toggle
+$('[data-app-dashboard-toggle-shrink]').on('click', function(e) {
+  e.preventDefault();
+  $(this).parents('.app-dashboard').toggleClass('shrink-medium').toggleClass('shrink-large');
+});
